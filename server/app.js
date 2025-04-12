c9a7137acd4c2d7366f37fd21da978d7c858662b
Mansi Ruhil <157950124+vimistify@users.noreply.github.com>
2025-04-12 23:46:09 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const cors = require('cors');
+
+require('./config/database');
+
+const auth = require('./routes/auth');
+const list = require('./routes/list');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+// setting up a middleware function for the authentication.
+app.use('/api/v1', auth);
+
+// setting up a middleware function for the todo list
+app.use('/api/v2', list);
+
+// starting the server that listens on port 8000.
+app.listen(8000, () => {
+    console.log("Server Started!!!");
+});
